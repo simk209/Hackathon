906659e588a3ca57f356073632b6ded5d66a28d1
jasony000 <jasony1004@gmail.com>
2023-06-07 20:34:23 -0400

    first draft

---
+++ main.js
@@ -0,0 +1,17 @@
+// let author
+// let quote;
+
+const quote = document.querySelector('.quotes');
+const author = document.querySelector('.author');
+const button = document.querySelector('.button');
+
+button.addEventListener('click', onclick);
+onclick();
+function onclick() {
+  fetch('https://api.quotable.io/random')
+    .then((response) => response.json())
+    .then((response) => {
+      quote.innerText = response.content;
+      author.innerText = response.author;
+    });
+}
